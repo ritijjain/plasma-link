1848591426f1de7da1dbdf223819965b3da054e2
Ritij Jain <ritij@ritijjain.com>
2021-04-28 23:37:04 +0530

    Debug set to env var

--- plasma_link_django/settings.py
+++ plasma_link_django/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ["DEBUG"])
 
 ALLOWED_HOSTS = ['plasmalink.herokuapp.com']
 
