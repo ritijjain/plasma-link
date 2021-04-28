f7e2480fd63e8f237b5c459b8ba592bdc8c7de8a
Ritij Jain <ritij@ritijjain.com>
2021-04-28 06:03:01 +0530

    Fixed syntax error in urls.py

--- plasma_link/urls.py
+++ plasma_link/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('donor-registration/', views.RegisterDonor.as_view(), name='donor_registration'),
     path('', views.FindDonor, name='find_donor'),
-    path('donor/<slug:pk>', views.donor_detail, name='donor_detail')
-    path('about/', view.AboutView.as_view(), name='about')
+    path('donor/<slug:pk>', views.donor_detail, name='donor_detail'),
+    path('about/', views.AboutView.as_view(), name='about'),
 ]
